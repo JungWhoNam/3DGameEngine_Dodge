a5138a271d5c8870a7834e1cf644cae418ec8cf8
Jung Who Nam <jungwhonam@gmail.com>
2026-03-24 10:11:17 +0900

    lec: create player part 2

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -7,30 +7,41 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            // 왼쪽 방향키 입력이 감지된 경우 z 방향 힘 주기
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            // 왼쪽 방향키 입력이 감지된 경우 -z 방향 힘 주기
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            // 왼쪽 방향키 입력이 감지된 경우 x 방향 힘 주기
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            // 왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = yInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+        playerRigidbody.linearVelocity = newVelocity;
+
+        //if (Input.GetKey(KeyCode.UpArrow) == true)
+        //{
+        //    // 왼쪽 방향키 입력이 감지된 경우 z 방향 힘 주기
+        //    playerRigidbody.AddForce(0f, 0f, speed);
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow) == true)
+        //{
+        //    // 왼쪽 방향키 입력이 감지된 경우 -z 방향 힘 주기
+        //    playerRigidbody.AddForce(0f, 0f, -speed);
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow) == true)
+        //{
+        //    // 왼쪽 방향키 입력이 감지된 경우 x 방향 힘 주기
+        //    playerRigidbody.AddForce(speed, 0f, 0f);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow) == true)
+        //{
+        //    // 왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
+        //    playerRigidbody.AddForce(-speed, 0f, 0f);
+        //}
 
         if (Input.GetKey(KeyCode.Space) == true)
         {
