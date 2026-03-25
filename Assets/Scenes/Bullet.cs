45359cacc25b3a9a001586f959461163552c00b5
Jung Who Nam <jungwhonam@gmail.com>
2026-03-25 12:08:47 +0900

    lec:add a bullet

---
+++ Assets/Scenes/Bullet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody bulletRigidbody;
+    private float speed = 8;
+
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.linearVelocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController controller = other.GetComponent<PlayerController>();
+            if (controller != null)
+            {
+                controller.Die();
+            }
+        }
+    }
+}
